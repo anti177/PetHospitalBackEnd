0e875087f0a0734a8470f3990daa01620cab271c
anti177 <1803697047@qq.com>
2023-03-17 10:23:25 +0800

    修改接口路径

--- src/main/java/com/example/pethospitalbackend/controller/RolePlayController.java
+++ src/main/java/com/example/pethospitalbackend/controller/RolePlayController.java
@@ -2,19 +2,15 @@ package com.example.pethospitalbackend.controller;
 
 import com.example.pethospitalbackend.dto.RoleDTO;
 import com.example.pethospitalbackend.dto.RoleProcessDTO;
-import com.example.pethospitalbackend.enums.ResponseEnum;
 import com.example.pethospitalbackend.response.Response;
 import com.example.pethospitalbackend.service.RolePlayService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -25,7 +21,7 @@ public class RolePlayController {
 	RolePlayService rolePlayService;
 
 	//1是医生，2是护士，3是前台
-	@GetMapping("/getRoleContentAndResponsibility")
+	@GetMapping("/RoleContentAndResponsibility")
 	@ApiOperation(value = "获得角色内容和职责")
 	public ResponseEntity<Response<RoleDTO>> getRoleContentAndResponsibility(@RequestParam("roleId") String roleId) {
 		Response<RoleDTO> response =  rolePlayService.getRoleContentAndResponsibility(roleId);
@@ -33,7 +29,7 @@ public class RolePlayController {
 	}
 
 
-	@GetMapping("/getRoleProcess")
+	@GetMapping("/RoleProcess")
 	@ApiOperation(value = "获得角色流程")
 	public ResponseEntity<Response<List<RoleProcessDTO>>> getRoleProcess(@RequestParam("roleId") String roleId) {
 		Response<List<RoleProcessDTO>> response =  rolePlayService.getRoleProcess(roleId);
