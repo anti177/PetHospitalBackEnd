ff49764e20efb0cf6d208b37127b466530538f31
anti177 <1803697047@qq.com>
2023-03-29 14:19:46 +0800

    实习返回药品、疫苗和科室信息的接口

---
+++ src/main/java/com/example/pethospitalbackend/dao/VaccineDao.java
@@ -0,0 +1,6 @@
+package com.example.pethospitalbackend.dao;
+
+import com.example.pethospitalbackend.entity.Vaccine;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface VaccineDao extends Mapper<Vaccine> {}
