d1c763ca5324dfa7835ec40adb5f77302371d9f0
shizuka <2358470826@qq.com>
2023-04-06 16:28:47 +0800

    modify update case function modify upload file function

--- src/main/java/com/example/pethospitalbackend/controller/UploadFileExampleController.java
+++ src/main/java/com/example/pethospitalbackend/controller/UploadFileExampleController.java
@@ -1,7 +1,7 @@
 package com.example.pethospitalbackend.controller;
 
 import com.example.pethospitalbackend.response.Response;
-import com.example.pethospitalbackend.service.VideoService;
+import com.example.pethospitalbackend.service.FileService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 使用oos上传文件的例子
@@ -22,25 +23,33 @@ import javax.annotation.Resource;
 @Api(tags = {"上传文件的例子"})
 public class UploadFileExampleController {
 
-  @Resource VideoService videoService;
+  @Resource FileService fileService;
 
   @PostMapping("/video")
-  @ApiOperation("添加视频")
+  @ApiOperation("添加视频测试")
   public ResponseEntity<Response<Boolean>> addVideo(
       @RequestParam("video_mp4") MultipartFile video_mp4) {
     Response<Boolean> response = new Response<>();
-    videoService.addVideo(video_mp4);
+    fileService.addVideo(video_mp4);
     response.setSuc(null);
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
 
   @PostMapping("/videos")
   @ApiOperation("添加多个视频")
-  public ResponseEntity<Response<Boolean>> addVideos(
-      @RequestParam("videos") MultipartFile[] videos) {
-    Response<Boolean> response = new Response<>();
-    videoService.addVideos(videos);
-    response.setSuc(null);
-    return new ResponseEntity<>(response, HttpStatus.OK);
+  public Response<List<String>> addVideos(@RequestParam("videos") MultipartFile[] videos) {
+    Response<List<String>> response = new Response<>();
+    List<String> urlList = fileService.addVideos(videos);
+    response.setSuc(urlList);
+    return response;
+  }
+
+  @PostMapping("/graphs")
+  @ApiOperation("添加多个图片")
+  public Response<List<String>> addGraphs(@RequestParam("graphs") MultipartFile[] graphs) {
+    Response<List<String>> response = new Response<>();
+    List<String> urlList = fileService.addGraphs(graphs);
+    response.setSuc(urlList);
+    return response;
   }
 }
