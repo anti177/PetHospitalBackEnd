1670e38a7682172e995752c1c1d24e8936446519
shizuka <2358470826@qq.com>
2023-04-07 08:41:31 +0800

    modify get question fields according to front end requirement

--- src/main/java/com/example/pethospitalbackend/dto/QuestionBackBriefDTO.java
+++ src/main/java/com/example/pethospitalbackend/dto/QuestionBackBriefDTO.java
@@ -8,12 +8,15 @@ public class QuestionBackBriefDTO {
   @ApiModelProperty(value = "题目id")
   private Long questionId;
 
-  @ApiModelProperty(value = "题目选项")
-  private String choice;
-
   @ApiModelProperty(value = "题目描述")
   private String description;
 
   @ApiModelProperty(value = "题目种类")
   private String questionType;
+
+  @ApiModelProperty(value = "关键词")
+  private String keyword;
+
+  @ApiModelProperty(value = "疾病名")
+  private String diseaseName;
 }
